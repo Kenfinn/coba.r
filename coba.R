af2729a72c8b9af9cb8f01cafcdbae5df0cd4e22
kenfinn <afinzaki.amiral@gmail.com>
2021-08-15 13:16:57 +0700

    first commit

---
+++ coba.R
@@ -0,0 +1,16 @@
+a<-25
+b<-10
+c<-a+b
+c
+
+#struktur data
+#scalar /atomic vector
+add_vector <- c(13,15,16,20,21)
+
+#list
+list1<- list("tes",12,"TRUE",15<9)
+
+list_2 <- list(colours = c("black", "yellow", "orange"), 
+               evaluation = c(TRUE, TRUE, FALSE, TRUE, FALSE, FALSE), 
+               time = matrix(1:6, nrow = 3))
+jml_siswa = 10 + 17
